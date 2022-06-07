cd1711400477a10f01466182750bfd46608be50a
Recidentable <el_maldito_GENIO@hotmail.com>
2022-06-07 15:07:27 -0400

    Ejercicio 3 xD

---
+++ 4.2.1ej3.py
@@ -0,0 +1,11 @@
+'''3. Crear un arreglo de dos dimensiones de 3 x 3 
+con números ceros, excepto la diagonal principal que 
+debe contener en el mismo orden los siguientes elementos 1, 2 y 3.
+'''
+
+import numpy as np
+
+matriz = np.diag([1,2,3]) 
+
+       
+print(matriz)
